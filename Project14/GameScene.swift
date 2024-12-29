64bbfbaa5ca45564653b6da620d94ee821a336f1
Maksim Li <maximlee.ml@gmail.com>
2024-12-29 22:58:29 +0200

    Initial Commit

---
+++ Project14/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Maksim Li on 29/12/2024.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+
+}
