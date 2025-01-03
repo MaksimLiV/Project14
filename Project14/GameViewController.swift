e75157cc6ecfae7bca2d89dba61e52d23483b8ef
Maksim Li <maximlee.ml@gmail.com>
2025-01-04 01:07:53 +0200

    new changes

--- Project14/GameViewController.swift
+++ Project14/GameViewController.swift
@@ -10,17 +10,14 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
                 scene.scaleMode = .fill
                 
-                // Present the scene
                 view.presentScene(scene)
             }
             
@@ -30,7 +27,7 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -38,7 +35,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
