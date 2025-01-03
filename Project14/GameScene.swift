3e5f1f28404a1be9dce6e2dfc0b8276b20cbe5c4
Maksim Li <maximlee.ml@gmail.com>
2025-01-04 00:42:19 +0200

    Adding effects smoke and mud

--- Project14/GameScene.swift
+++ Project14/GameScene.swift
@@ -59,8 +59,7 @@ class GameScene: SKScene {
                 score -= 5
                 
                 run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
-            } else if node.name == "charEnemy"{
-                
+            } else if node.name == "charEnemy" {
                 whackSlot.charNode?.xScale = 0.85
                 whackSlot.charNode?.yScale = 0.85
                 score += 1
@@ -72,13 +71,12 @@ class GameScene: SKScene {
     
     func createSlot(at position: CGPoint) {
         let slot = WhackSlot()
-        slot.configure(at:position)
+        slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
     }
     
     func createEnemy() {
-        
         numRounds += 1
         
         if numRounds >= 30 {
@@ -101,12 +99,12 @@ class GameScene: SKScene {
             addChild(finalScoreLabel)
             
             return
-            
         }
+        
         popupTime *= 0.991
         
         slots.shuffle()
-        slots [0].show(hideTime: popupTime)
+        slots[0].show(hideTime: popupTime)
         
         if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
         if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
@@ -120,5 +118,71 @@ class GameScene: SKScene {
         DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
             self?.createEnemy()
         }
+    }
+    
+    func createSmokeEffect(at position: CGPoint) -> SKEmitterNode {
+        let smoke = SKEmitterNode()
+        smoke.particleTexture = SKTexture(imageNamed: "spark")
+        smoke.position = position
+        smoke.zPosition = 1
+        
+        smoke.particleBirthRate = 100
+        smoke.numParticlesToEmit = 40
+        smoke.particleLifetime = 1.0
+        smoke.particleLifetimeRange = 0.5
+        
+        smoke.particleColor = .gray
+        smoke.particleColorBlendFactor = 1
+        smoke.particleColorBlendFactorRange = 0.3
+        smoke.particleAlpha = 0.4
+        smoke.particleAlphaRange = 0.3
+        smoke.particleAlphaSpeed = -0.2
+        
+   
+        smoke.particleSpeed = 80
+        smoke.particleSpeedRange = 20
+        smoke.emissionAngle = -.pi / 2
+        smoke.emissionAngleRange = .pi / 2
+        smoke.xAcceleration = 0
+        smoke.yAcceleration = 15
+        
+        smoke.particleScale = 0.5
+        smoke.particleScaleRange = 0.2
+        smoke.particleScaleSpeed = 0.4
+        
+        return smoke
+    }
+
+    func createMudEffect(at position: CGPoint) -> SKEmitterNode {
+        let mud = SKEmitterNode()
+        mud.particleTexture = SKTexture(imageNamed: "spark")
+        mud.position = position
+        mud.zPosition = 1
+        
+        mud.particleBirthRate = 200
+        mud.numParticlesToEmit = 30
+        mud.particleLifetime = 0.8
+        mud.particleLifetimeRange = 0.4
+        
+        mud.particleColor = .brown
+        mud.particleColorBlendFactor = 1
+        mud.particleColorBlendFactorRange = 0.2
+        mud.particleAlpha = 0.7
+        mud.particleAlphaRange = 0.3
+        mud.particleAlphaSpeed = -0.8
+        
+        mud.particleSpeed = 50
+        mud.particleSpeedRange = 20
+        mud.emissionAngle = 0
+        mud.emissionAngleRange = .pi * 2
+        mud.xAcceleration = 0
+        mud.yAcceleration = -150
+        
+
+        mud.particleScale = 0.4
+        mud.particleScaleRange = 0.2
+        mud.particleScaleSpeed = -0.2
+        
+        return mud
     }
 }
--- Project14/WhackSlot.swift
+++ Project14/WhackSlot.swift
@@ -39,6 +39,18 @@ class WhackSlot: SKNode {
         charNode.xScale = 1
         charNode.yScale = 1
         
+        // Add mud effect when coming out
+        if let gameScene = self.scene as? GameScene {
+            let mudEffect = gameScene.createMudEffect(at: CGPoint(x: 0, y: -50))
+            mudEffect.numParticlesToEmit = 15
+            addChild(mudEffect)
+            
+            // Remove the mud effect after animation
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                mudEffect.removeFromParent()
+            }
+        }
+        
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
@@ -51,14 +63,25 @@ class WhackSlot: SKNode {
             charNode.name = "charEnemy"
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + hideTime * 3.5) {
-            [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + hideTime * 3.5) { [weak self] in
             self?.hide()
         }
     }
     
     func hide() {
         if !isVisible { return }
+        
+        // Add mud effect when going back in
+        if let gameScene = self.scene as? GameScene {
+            let mudEffect = gameScene.createMudEffect(at: CGPoint(x: 0, y: -50))
+            mudEffect.numParticlesToEmit = 15
+            addChild(mudEffect)
+            
+            // Remove the mud effect after animation
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                mudEffect.removeFromParent()
+            }
+        }
         
         charNode.run(SKAction.move(to: CGPoint(x: 0, y: -80), duration: 0.05))
         isVisible = false
@@ -67,11 +90,21 @@ class WhackSlot: SKNode {
     func hit() {
         isHit = true
         
+        // Add smoke effect when hit
+        if let gameScene = self.scene as? GameScene {
+            let smokeEffect = gameScene.createSmokeEffect(at: CGPoint(x: 0, y: 0))
+            addChild(smokeEffect)
+            
+            // Remove the smoke effect after animation
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                smokeEffect.removeFromParent()
+            }
+        }
+        
         let delay = SKAction.wait(forDuration: 0.25)
         let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
-        let notVisible = SKAction.run { [weak self] in self?.isVisible = false}
+        let notVisible = SKAction.run { [weak self] in self?.isVisible = false }
         let sequence = SKAction.sequence([delay, hide, notVisible])
         charNode.run(sequence)
     }
 }
-
